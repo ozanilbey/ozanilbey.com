6c8815aff541cd7628514c3b19f24f67d8f95aff
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-08-24 11:24:47 +0300

    Added new icons

--- source/components/interface/icon/Icon.js
+++ source/components/interface/icon/Icon.js
@@ -5,7 +5,9 @@ import PropTypes from 'prop-types'
 // Shapes
 import Contrast from './shapes/Contrast'
 import Exit from './shapes/Exit'
+import GitHub from './shapes/GitHub'
 import HamburgerMenu from './shapes/HamburgerMenu'
+import Star from './shapes/Star'
 
 // Interface: Icon
 const Icon = props => {
@@ -14,8 +16,12 @@ const Icon = props => {
       return <Contrast {...props} />
     case 'exit':
       return <Exit {...props} />
+    case 'github':
+      return <GitHub {...props} />
     case 'hamburger-menu':
       return <HamburgerMenu {...props} />
+    case 'star':
+      return <Star {...props} />
   }
 }
 
