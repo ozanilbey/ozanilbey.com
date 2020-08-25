212b74266501e8e1a8d86f188b024af7bd8ec334
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-08-25 15:26:41 +0300

    Improved Card component

--- source/components/interface/card/Card.js
+++ source/components/interface/card/Card.js
@@ -2,6 +2,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// Components
+import Icon from '~/components/interface/icon/Icon'
+
 // Utilities
 import { getClassName } from '~/utilities/component'
 
@@ -10,11 +13,28 @@ import './Card.less'
 
 // Component: Card
 function Card (props) {
-  const className = getClassName(props.className)
+  const className = getClassName(props.className, { layout: props.layout })
   return (
     <div
       data-interface="card"
-      className={className}>
+      className={className}
+      style={props.style}>
+      {props.icon &&
+        <Icon
+          name={props.icon}
+          className="visual" />
+      }
+      {props.image &&
+        <img
+          src={props.image}
+          className="visual" />
+      }
+      {(props.title || props.description) &&
+        <div className="text">
+          {props.title && <strong className="title">{props.title}</strong>}
+          {props.description && <p className="description">{props.description}</p>}
+        </div>
+      }
       {props.children}
     </div>
   )
@@ -24,7 +44,13 @@ function Card (props) {
 Card.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  color: PropTypes.string
+  color: PropTypes.string,
+  description: PropTypes.string,
+  icon: PropTypes.string,
+  image: PropTypes.string,
+  layout: PropTypes.oneOf(['horizontal', 'vertical']),
+  style: PropTypes.object,
+  title: PropTypes.string
 }
 
 // Export
