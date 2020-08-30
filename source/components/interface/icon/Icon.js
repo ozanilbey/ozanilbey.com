5df2273c760351265d43184fc540089731c24334
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-08-30 15:45:08 +0300

    Updated icons

--- source/components/interface/icon/Icon.js
+++ source/components/interface/icon/Icon.js
@@ -3,30 +3,46 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 // Subcomponents
+import Audio from './shapes/Audio'
 import Contrast from './shapes/Contrast'
 import Exit from './shapes/Exit'
 import GitHub from './shapes/GitHub'
 import HamburgerMenu from './shapes/HamburgerMenu'
 import Star from './shapes/Star'
 
+// Utilities
+import { getAttributes } from '~/utilities/component'
+
 // Component: Interface > Icon
 const Icon = props => {
+  // Data
+  const attributes = {
+    'data-interface': 'icon',
+    'data-icon': props.name,
+    'data-color': props.color,
+    ...getAttributes(props, ['className', 'style', 'data', 'aria'])
+  }
+
+  // Render
   switch (props.name) {
+    case 'audio':
+      return <Audio {...attributes} />
     case 'contrast':
-      return <Contrast {...props} />
+      return <Contrast {...attributes} />
     case 'exit':
-      return <Exit {...props} />
+      return <Exit {...attributes} />
     case 'github':
-      return <GitHub {...props} />
+      return <GitHub {...attributes} />
     case 'hamburger-menu':
-      return <HamburgerMenu {...props} />
+      return <HamburgerMenu {...attributes} />
     case 'star':
-      return <Star {...props} />
+      return <Star {...attributes} />
   }
 }
 
 // Properties
 Icon.propTypes = {
+  color: PropTypes.string,
   name: PropTypes.string.isRequired
 }
 
