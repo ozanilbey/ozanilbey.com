a74e4a68b0e1c97944bcbaf0f12558599910dffc
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-09-01 18:38:07 +0300

    Added getSlug utility function

--- source/utilities/format.js
+++ source/utilities/format.js
@@ -1,3 +1,10 @@
 // Get two-digit number
 export const getTwoDigitNumber = integer =>
   ('0' + integer).slice(-2)
+
+// Get slug
+export const getSlug = text =>
+  text
+    .trim()
+    .replace(/[\s\W-]+/g, '-')
+    .toLowerCase()
