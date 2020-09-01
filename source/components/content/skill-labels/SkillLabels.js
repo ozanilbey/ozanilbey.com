faf461d5ea98f4638551fd72ea8b7f1d8213232d
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-09-01 18:39:49 +0300

    Added SkillLabels component

---
+++ source/components/content/skill-labels/SkillLabels.js
@@ -0,0 +1,54 @@
+// Modules
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// Utilities
+import { getClassName } from '~/utilities/component'
+
+// Constants
+import { LEVEL_OPTIONS } from '~/constants/options'
+
+// Style
+import './SkillLabels.less'
+
+// Component: Content > SkillLabels
+function SkillLabels (props) {
+  // Render
+  return (
+    <div data-content="skill-labels">
+      <ul className="labels">
+        {props.data.map((skill, index) =>
+          <li key={index}>
+            <dl>
+              <dt>{skill.name}</dt>
+              {skill.items && skill.items.map((item, index) =>
+                <dd
+                  key={index}
+                  data-level={LEVEL_OPTIONS[item.level]}>
+                  {item.name}
+                </dd>
+              )}
+            </dl>
+          </li>
+        )}
+      </ul>
+      <ul className="legend">
+        {LEVEL_OPTIONS.map((level, index) =>
+          <li
+            key={index}
+            data-level={LEVEL_OPTIONS[index]}>
+            {level}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+// Properties
+SkillLabels.propTypes = {
+  data: PropTypes.array
+}
+
+// Export
+export default SkillLabels
