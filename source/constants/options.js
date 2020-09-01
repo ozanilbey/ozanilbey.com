e3a50f9d0a1eed58915748d30096b95be5978828
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-09-01 18:42:18 +0300

    Added advancement levels as constant

--- source/constants/options.js
+++ source/constants/options.js
@@ -1,3 +1,4 @@
 export const NAVIGATION_MENU_OPTIONS = ['home', 'profile', 'works', 'contact']
 export const THEME_OPTIONS = ['light', 'dark']
 export const COLOR_OPTIONS = ['green', 'yellow', 'pink', 'purple', 'blue']
+export const LEVEL_OPTIONS = ['novice', 'intermediate', 'advanced']
