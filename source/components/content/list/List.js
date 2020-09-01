bc1f0bd3a7f41c11c0d4779af14e67f7a2450d33
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-09-01 18:39:06 +0300

    Added List component

---
+++ source/components/content/list/List.js
@@ -0,0 +1,32 @@
+// Modules
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+// Utilities
+import { getClassName } from '~/utilities/component'
+
+// Style
+import './List.less'
+
+// Component: Content > List
+function List (props) {
+  // Render
+  return (
+    <ul
+      data-content="list"
+      className={getClassName(props.className, { lead: true })}>
+      {props.data.map((item, index) =>
+        <li key={index}>{item}</li>
+      )}
+    </ul>
+  )
+}
+
+// Properties
+List.propTypes = {
+  className: PropTypes.object,
+  data: PropTypes.array
+}
+
+// Export
+export default List
