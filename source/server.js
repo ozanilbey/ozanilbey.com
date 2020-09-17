c93a8c56d0a332977fc709ad55b2bbf6b16566d6
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-09-17 21:13:06 +0300

    Added mailer

--- source/server.js
+++ source/server.js
@@ -2,6 +2,8 @@
 import { readFileSync } from 'fs'
 import http from 'http'
 import express from 'express'
+import dotenv from 'dotenv'
+import nodemailer from 'nodemailer'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
@@ -17,6 +19,18 @@ const server = new http.Server(app)
 
 // HTML template
 const template = readFileSync('public/index.html', 'utf8')
+
+// Add environment variables
+dotenv.config()
+
+// Configure email system
+const mailer = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASSWORD
+  }
+})
 
 // Serve JavaScript files
 app.get('*.js', (request, response, next) => {
@@ -38,6 +52,53 @@ app.get('*.css', (request, response, next) => {
 
 // Serve static files
 app.use('/', express.static('public'))
+
+// Handle emails
+app.use('/email', express.json())
+app.post('/email', (request, response) => {
+  // Get necessary data
+  const { name, address, message } = request.body
+  // Set a generic response function
+  const respond = (message, isError) => {
+    response
+      .status(isError ? 500 : 200)
+      .send(message)
+  }
+  // Check if the necessary information is valid
+  if (
+    name &&
+    address &&
+    message &&
+    [name, address, message].filter(item => typeof item !== 'string').length === 0
+  ) {
+    // Try sending email
+    try {
+      // Set options
+      const options = {
+        from: process.env.EMAIL_SENDER,
+        to: process.env.EMAIL_RECEIVER,
+        replyTo: request.body.address,
+        subject: `Email from ${request.body.name}`,
+        text: request.body.message,
+        priority: 'high'
+      }
+      // Send email
+      mailer.sendMail(options, (error, info) => {
+        // Respond with related error
+        if (error) respond('Something went wrong.', true)
+        // Respond with a message
+        else respond('Email has been sent.')
+      })
+    } catch (error) {
+      // Respond with a generic error
+      respond('Something went wrong.', true)
+    }
+  } else {
+    // Respond with missing information error
+    respond('Name, email address or message is missing.', true)
+    return false
+  }
+})
 
 // Handle all other requests
 app.use((request, response) => {
