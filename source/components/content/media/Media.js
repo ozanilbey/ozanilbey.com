937f451de96ea89a926ea36a4df62a211230bcdb
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-09-28 14:57:09 +0300

    Added Media component

---
+++ source/components/content/media/Media.js
@@ -0,0 +1,64 @@
+// Modules
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// Utilities
+import { getAttributes } from '~/utilities/component'
+
+// Constants
+import { MEDIA_ENDPOINT } from '~/constants/settings'
+import { MEDIA_TYPE_OPTIONS } from '~/constants/options'
+
+// Style
+import './Media.less'
+
+// Component: Content > Media
+function Media (props) {
+  // Data
+  const attributes = {
+    'data-content': 'media',
+    'data-media': props.type,
+    ...getAttributes(props, ['className', 'style', 'alt', 'title', 'data', 'aria'])
+  }
+
+  // Methods
+  function getFilters () {
+    const filters = []
+    if (props.width || props.height) filters.push('c_limit')
+    if (props.width) filters.push(`w_${props.width}`)
+    if (props.height) filters.push(`h_${props.height}`)
+    return filters.length > 0 ? `${filters.join(',')}` : ''
+  }
+  function getURL () {
+    const filters = getFilters()
+    return `${MEDIA_ENDPOINT}/${filters}${props.source}`
+  }
+
+  // Render
+  if (!props.source) return null
+  switch (props.type) {
+    case 'image':
+      return (
+        <img
+          {...attributes}
+          src={getURL()} />
+      )
+    case 'video':
+      return null
+    case 'document':
+      return null
+  }
+}
+
+// Properties
+Media.propTypes = {
+  className: PropTypes.string,
+  filters: PropTypes.object,
+  height: PropTypes.number,
+  source: PropTypes.string,
+  type: PropTypes.oneOf(MEDIA_TYPE_OPTIONS),
+  width: PropTypes.number
+}
+
+// Export
+export default Media
