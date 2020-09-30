f8f4b0b1a1a78515e058bf46e4389d962b42e502
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-10-01 00:28:28 +0300

    Revised work type data

--- source/constants/content.js
+++ source/constants/content.js
@@ -1,4 +1,4 @@
-export const WORK_TYPES = ['collection', 'corporate-identity', 'desktop-website', 'graphic-design', 'mobile-application', 'responsive-website', 'social-media']
+export const WORK_COVER_TYPES = ['Collection', 'Corporate Identity', 'Graphic Design', 'Mobile Application', 'Responsive Website', 'Social Media', 'Website']
 export const SUMMARY_TYPES = ['work', 'employment', 'education']
 export const FEATURED_WORKS = ['docu', 'graphjs', 'base64', 'favogue', 'piccture', 'cardz']
 export const SELECTED_WORKS = [...FEATURED_WORKS, 'brush', 'fihrist', 'groups']
