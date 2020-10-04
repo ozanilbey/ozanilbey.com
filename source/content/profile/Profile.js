0030df93fa34a6b0487494db844beab122cd1a3a
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-10-04 18:26:37 +0300

    Added Profile content

---
+++ source/content/profile/Profile.js
@@ -0,0 +1,67 @@
+// Modules
+import React from 'react'
+
+// Components
+import Container from '~/components/layout/container/Container'
+import Page from '~/components/layout/page/Page'
+import Headline from '~/components/model/headline/Headline'
+import SlidingBox from '~/components/model/sliding-box/SlidingBox'
+import Link from '~/components/interface/link/Link'
+import Heading from '~/components/type/heading/Heading'
+import SkillMap from '~/components/content/skill-map/SkillMap'
+
+// Data
+import allSkills from '~/data/allSkills'
+
+// Style
+import './Profile.less'
+
+// Content: Profile
+function Profile () {
+  // Render
+  return (
+    <Page.Section
+      name="profile"
+      data-content="profile">
+      <Container>
+        <Heading
+          isSingleLine
+          className="dim"
+          rank={4}>
+          I’m basically a
+        </Heading>
+        <Headline
+          isSingleLine
+          displaySize="big">
+          designer
+          <mark>+</mark>
+          developer
+        </Headline>
+        <p className="lead">I have a wide range of design experience, from <strong>branding</strong> to <strong>animation</strong>, even <strong>printmaking</strong>. Plus, I’m able to create <strong>functional UIs</strong>, and develop <strong>full-stack JavaScript applications</strong> on Node with GraphQL APIs and React (Native) mobile/web clients. <Link type="cta" to="/profile">Read more</Link></p>
+      </Container>
+      <Container>
+        <SlidingBox>
+          <SkillMap
+            isLeftContinuous
+            isRightContinuous
+            unitSize={8}
+            sections={[
+              { name: 'Design', range: [0, 5] },
+              { name: 'Development', range: [5, 9] }
+            ]}
+            skills={
+              allSkills
+                .map(item => ({
+                  name: item.category,
+                  description: item.skills.join(', ')
+                }))
+                .concat(['Testing & QA', 'DevOps'].map(name => ({ name, isDisabled: true })))
+            } />
+        </SlidingBox>
+      </Container>
+    </Page.Section>
+  )
+}
+
+// Export
+export default Profile
