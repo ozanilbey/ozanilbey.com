35de12db8ef86db98703a270140470ff338e61ae
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-10-04 18:28:00 +0300

    Added Contact content

---
+++ source/content/contact/Contact.js
@@ -0,0 +1,40 @@
+// Modules
+import React from 'react'
+
+// Components
+import Container from '~/components/layout/container/Container'
+import Page from '~/components/layout/page/Page'
+import Headline from '~/components/model/headline/Headline'
+import Link from '~/components/interface/link/Link'
+import Heading from '~/components/type/heading/Heading'
+
+// Style
+import './Contact.less'
+
+// Content: Contact
+function Contact () {
+  // Render
+  return (
+    <Page.Section
+      name="contact"
+      data-content="Contact">
+      <Container>
+        <Heading
+          isSingleLine
+          className="dim"
+          rank={4}>
+          Well,
+        </Heading>
+        <Headline
+          isSingleLine
+          displaySize="big">
+          let’s talk
+        </Headline>
+        <p className="lead">If you have something to say or a job to offer, I’d like to hear from you. <Link type="cta" to="/contact">Contact me</Link></p>
+      </Container>
+    </Page.Section>
+  )
+}
+
+// Export
+export default Contact
