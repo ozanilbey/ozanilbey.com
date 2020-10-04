cc8992f228a84c52cb27353caabc1839ecf1401e
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-10-04 17:08:27 +0300

    Added Showcase component

---
+++ source/components/content/showcase/Showcase.js
@@ -0,0 +1,93 @@
+// Modules
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+// Components
+import Link from '~/components/interface/link/Link'
+import Card from '~/components/interface/card/Card'
+import WorkBrand from '~/components/content/work-brand/WorkBrand'
+import WorkCover from '~/components/content/work-cover/WorkCover'
+
+// Utilities
+import { slug, rgbColor } from '~/utilities/format'
+
+// Style
+import './Showcase.less'
+
+// Component: Content > Showcase
+function Showcase (props) {
+  // State
+  const [list, setList] = useState([])
+
+  // Effects
+  useEffect(() => {
+    function sortItems () {
+      return props.data.sort((x, y) => {
+        return (x.isFeatured === y.isFeatured)
+          ? 0
+          : x ? -1 : 1
+      })
+    }
+    setList(sortItems(props.data))
+  }, [props.data])
+
+  // Render
+  return (
+    <div data-content="showcase">
+      {list.map(item =>
+        <Link
+          key={item.slug}
+          to={`/works/${item.slug}`}>
+          <Card
+            isCustom
+            layout="horizontal"
+            color={rgbColor(item.colors.primary)}>
+            <div className="visual">
+              <WorkCover
+                types={item.preview.map(item => slug(item))}
+                data={item} />
+            </div>
+            <div className="content">
+              <WorkBrand
+                size={90}
+                data={item} />
+              <div
+                className="text"
+                style={{ color: item.colors.secondary && rgbColor(item.colors.secondary) }}>
+                <ul className="description">
+                  {item.type === 'project'
+                    ? <li data-label="Description">{item.description}</li>
+                    : <li data-label="Client">
+                      {item.type === 'work' ? item.client.name : 'Various Clients'}
+                    </li>
+                  }
+                  <li data-label="Year">
+                    {typeof item.year === 'number' ? item.year : item.year.join('–')}
+                  </li>
+                </ul>
+              </div>
+              {item.tags &&
+                <ul
+                  className="tags"
+                  style={{ color: rgbColor(item.colors.primary) }}>
+                  {item.tags.map((tag, index) =>
+                    <li key={index}>{tag}</li>
+                  )}
+                </ul>
+              }
+            </div>
+          </Card>
+        </Link>
+      )}
+    </div>
+  )
+}
+
+// Properties
+Showcase.propTypes = {
+  data: PropTypes.array,
+  className: PropTypes.string
+}
+
+// Export
+export default Showcase
