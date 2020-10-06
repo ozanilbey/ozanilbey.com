5a05a0bb5fbf8d73886abc8dedc8f7ba8e0784cc
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-10-06 19:58:33 +0300

    Updated Works page content

--- source/pages/works/Works.js
+++ source/pages/works/Works.js
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
 
 // Components
-import Container from '~/components/layout/container/Container'
-import Heading from '~/components/type/heading/Heading'
 import Page from '~/components/layout/page/Page'
 
 // Content
 import Meta from '~/content/meta/Meta'
+
+// Subcontent
+import Introduction from './content/introduction/Introduction'
+import Portfolio from './content/portfolio/Portfolio'
 
 // Subpages
 import Work from './work/Work'
@@ -35,6 +37,7 @@ function Works () {
   const { workOrFilter } = useParams()
   const workData = useWork(getWorkIndex(workOrFilter) > -1 ? workOrFilter : null)
   const selectedWorksLabel = WORK_FILTERS[0]
+  const filter = getFilter()
 
   // State
   const [workSet, setWorkSet] = useState(SELECTED_WORKS)
@@ -89,21 +92,19 @@ function Works () {
     : <Page name="works">
       <Meta
         page="works"
-        category={getFilter() === selectedWorksLabel ? null : getFilter()} />
-      <Page.Header name="introduction">
-        <Container>
-          <Heading displaySize="big">Works</Heading>
-          <p>This is <b>Works</b> page.</p>
-        </Container>
-      </Page.Header>
-      <Page.Section>
-        <Container>
-          <b>All works</b>
-          <ul>
-            <li><Link to="/works/sample">Sample Work</Link></li>
-          </ul>
-        </Container>
-      </Page.Section>
+        category={filter === selectedWorksLabel ? null : filter} />
+      <Introduction />
+      <Portfolio
+        filter={filter}
+        filters={WORK_FILTERS}
+        onFilter={filterWorks}
+        works={
+          workSet.length > 0
+            ? works
+              .filter(work => workSet.includes(work.slug))
+              .sort((x, y) => workSet.indexOf(x.slug) - workSet.indexOf(y.slug))
+            : null
+        } />
     </Page>
 }
 
