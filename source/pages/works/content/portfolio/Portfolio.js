cd415ef549ff75dcfd36d7cc3614aa86f077082a
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-10-06 19:54:05 +0300

    Added Portfolio subcontent

---
+++ source/pages/works/content/portfolio/Portfolio.js
@@ -0,0 +1,64 @@
+// Modules
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// Components
+import Headline from '~/components/model/headline/Headline'
+import Page from '~/components/layout/page/Page'
+import Container from '~/components/layout/container/Container'
+import Dropdown from '~/components/interface/dropdown/Dropdown'
+import Heading from '~/components/type/heading/Heading'
+import Showcase from '~/components/content/showcase/Showcase'
+
+// Style
+import './Portfolio.less'
+
+// Subcontent: Works > Portfolio
+function Portfolio (props) {
+  // Render
+  return (
+    <Page.Section
+      name="portfolio"
+      data-content="portfolio">
+      <Container isBlockLayout>
+        <Heading
+          isSingleLine
+          className="dim"
+          rank={4}>
+          Take a look at my
+        </Heading>
+        <Headline
+          isSingleLine
+          displaySize="big">
+          design portfolio
+        </Headline>
+        <p className="lead">Here are some of my projects expressing my design approach. To see more, filter with another category.</p>
+        <div className="lead">
+          Currently displaying:
+          <Dropdown
+            side="right"
+            options={props.filters}
+            defaultValue={props.filter}
+            onChange={props.onFilter} />
+        </div>
+      </Container>
+      <Container>
+        {
+          props.works
+            ? <Showcase data={props.works} />
+            : <p>There is no work matching your criteria.</p>
+        }
+      </Container>
+    </Page.Section>
+  )
+}
+
+Portfolio.propTypes = {
+  filter: PropTypes.string,
+  filters: PropTypes.array,
+  onFilter: PropTypes.func,
+  works: PropTypes.array
+}
+
+// Export
+export default Portfolio
