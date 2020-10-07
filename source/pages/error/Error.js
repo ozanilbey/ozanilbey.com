445b3eae87ad64636119ae41221894f5b1b401e5
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-10-07 16:11:38 +0300

    Updated Error page content

--- source/pages/error/Error.js
+++ source/pages/error/Error.js
@@ -1,11 +1,11 @@
 // Modules
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 // Components
 import Container from '~/components/layout/container/Container'
-import Heading from '~/components/type/heading/Heading'
 import Page from '~/components/layout/page/Page'
+import Link from '~/components/interface/link/Link'
 
 // Content
 import Meta from '~/content/meta/Meta'
@@ -23,12 +23,16 @@ function Error () {
     <Page name="error">
       <Meta page="error" />
       <Page.Header
-        isFullScreen
-        name="introduction">
-        <Container>
-          <Heading displaySize="big">Error</Heading>
-          <p><mark>{pathname}</mark> cannot be found!</p>
-          <p>This is <b>Error</b> page. Let‘s go to <Link to="/">Home</Link> page.</p>
+        isCentered
+        name="introduction"
+        data-content="introduction">
+        <Container isBlockLayout>
+          <img src="/images/illustrations/error.png" />
+        </Container>
+        <Container isBlockLayout>
+          <mark>{pathname}</mark>
+          <p className="lead"><strong>Page cannot be found!</strong></p>
+          <Link type="cta" to="/">Let‘s go home</Link>
         </Container>
       </Page.Header>
     </Page>
