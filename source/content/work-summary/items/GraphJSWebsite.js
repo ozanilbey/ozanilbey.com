e3e131c463fe90dbc5e5fa7f17ed4ea3e27191d6
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-10-23 18:20:44 +0300

    Added GraphJS Website work

---
+++ source/content/work-summary/items/GraphJSWebsite.js
@@ -0,0 +1,123 @@
+// Modules
+import React from 'react'
+
+// Components
+import Container from '~/components/layout/container/Container'
+import Link from '~/components/interface/link/Link'
+import Heading from '~/components/type/heading/Heading'
+import Information from '~/components/type/information/Information'
+import Media from '~/components/content/media/Media'
+
+// Content: Work Summary > GraphJS Website (Item)
+const GraphJSWebsite = () => (
+  <>
+    <Container isBlockLayout>
+      <Media type="image" width={320} source="/graphjs-website/badge.png" />
+      <Media type="image" width={864} source="/graphjs-website/teaser.png" />
+      <p className="lead">GraphJS (formerly “Graph.js“) is an amazing tool for <strong>making any website social</strong> with just a few lines of code. It’s an <strong>open source client-side JavaScript library</strong>. It helps you to easily add authentication, comments, messages, forum, groups, profiles and many more to static web pages.</p>
+      <p className="lead">I had created the “Graph.js“ brand; designed, developed and maintained its website in <strong>2018–2019</strong>.</p>
+      <p className="lead">Special thanks to <Link type="external" to="https://cerenmurgan.com">Ceren</Link> for beautiful illustrations!</p>
+      <Information>I had also designed and developed the <Link type="regular" to="/works/graphjs">GraphJS</Link> library itself.</Information>
+    </Container>
+    <Container isBlockLayout>
+      <Heading rank={2}>Logo</Heading>
+      <Media type="image" width={640} source="/graphjs-website/01.png" />
+    </Container>
+    <Container isBlockLayout>
+      <Heading rank={2}>Slogan</Heading>
+      <Media type="image" width={640} source="/graphjs-website/02.png" />
+    </Container>
+    <Container isBlockLayout>
+      <Heading rank={2}>Mascot</Heading>
+      <Media type="image" width={240} source="/graphjs-website/03.png" />
+      <Media type="image" width={640} source="/graphjs-website/04.png" />
+      <small>The idea</small>
+    </Container>
+    <Container isBlockLayout>
+      <Heading rank={2}>Headers</Heading>
+      <Media type="image" width={1152} source="/graphjs-website/05.png" />
+      <small>Home</small>
+      <Media type="image" width={1152} source="/graphjs-website/06.png" />
+      <small>Career</small>
+      <Media type="image" width={1152} source="/graphjs-website/07.png" />
+      <small>Error</small>
+    </Container>
+    <Container isBlockLayout>
+      <Heading rank={2}>Desktop Screens</Heading>
+      <Media type="image" width={1152} source="/graphjs-website/08.png" />
+      <small>Home</small>
+      <Media type="image" width={1152} source="/graphjs-website/09.png" />
+      <small>Docs</small>
+      <Media type="image" width={1152} source="/graphjs-website/10.png" />
+      <small>Docs item</small>
+      <Media type="image" width={1152} source="/graphjs-website/11.png" />
+      <small>Get Started (URL)</small>
+      <Media type="image" width={1152} source="/graphjs-website/12.png" />
+      <small>Get Started (Theme)</small>
+      <Media type="image" width={1152} source="/graphjs-website/13.png" />
+      <small>Get Started (Color)</small>
+      <Media type="image" width={1152} source="/graphjs-website/14.png" />
+      <small>Get Started (Credentials)</small>
+      <Media type="image" width={1152} source="/graphjs-website/15.png" />
+      <small>Get Started (Payment)</small>
+      <Media type="image" width={1152} source="/graphjs-website/16.png" />
+      <small>Get Started (Done)</small>
+      <Media type="image" width={1152} source="/graphjs-website/17.png" />
+      <small>Login</small>
+      <Media type="image" width={1152} source="/graphjs-website/18.png" />
+      <small>Settings (URL)</small>
+      <Media type="image" width={1152} source="/graphjs-website/19.png" />
+      <small>Settings (Theme)</small>
+      <Media type="image" width={1152} source="/graphjs-website/20.png" />
+      <small>Settings (Color)</small>
+      <Media type="image" width={1152} source="/graphjs-website/21.png" />
+      <small>Settings (Code)</small>
+      <Media type="image" width={1152} source="/graphjs-website/22.png" />
+      <small>Settings (Credentials)</small>
+      <Media type="image" width={1152} source="/graphjs-website/23.png" />
+      <small>Settings (Subscriptions)</small>
+    </Container>
+    <Container isBlockLayout>
+      <Heading rank={2}>Mobile Screens</Heading>
+      <Media type="image" width={1152} source="/graphjs-website/24.png" />
+      <small>Home</small>
+      <Media type="image" width={1152} source="/graphjs-website/25.png" />
+      <small>Mobile menu</small>
+      <Media type="image" width={1152} source="/graphjs-website/26.png" />
+      <small>Docs</small>
+      <Media type="image" width={1152} source="/graphjs-website/27.png" />
+      <small>Docs (Mobile menu)</small>
+      <Media type="image" width={1152} source="/graphjs-website/28.png" />
+      <small>Docs item</small>
+      <Media type="image" width={1152} source="/graphjs-website/29.png" />
+      <small>Get Started (URL)</small>
+      <Media type="image" width={1152} source="/graphjs-website/30.png" />
+      <small>Get Started (Theme)</small>
+      <Media type="image" width={1152} source="/graphjs-website/31.png" />
+      <small>Get Started (Color)</small>
+      <Media type="image" width={1152} source="/graphjs-website/32.png" />
+      <small>Get Started (Credentials)</small>
+      <Media type="image" width={1152} source="/graphjs-website/33.png" />
+      <small>Get Started (Payment)</small>
+      <Media type="image" width={1152} source="/graphjs-website/34.png" />
+      <small>Get Started (Done)</small>
+      <Media type="image" width={1152} source="/graphjs-website/35.png" />
+      <small>Login</small>
+      <Media type="image" width={1152} source="/graphjs-website/36.png" />
+      <small>Settings (URL)</small>
+      <Media type="image" width={1152} source="/graphjs-website/37.png" />
+      <small>Settings (Theme)</small>
+      <Media type="image" width={1152} source="/graphjs-website/38.png" />
+      <small>Settings (Color)</small>
+      <Media type="image" width={1152} source="/graphjs-website/39.png" />
+      <small>Settings (Code)</small>
+      <Media type="image" width={1152} source="/graphjs-website/40.png" />
+      <small>Settings (Credentials)</small>
+      <Media type="image" width={1152} source="/graphjs-website/41.png" />
+      <small>Settings (Subscriptions)</small>
+    </Container>
+  </>
+)
+
+// Export
+export default GraphJSWebsite
