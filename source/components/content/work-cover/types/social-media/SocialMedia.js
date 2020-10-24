e8be9dec45bf99a8459fe8bf1eb2abd4736bd92b
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2020-10-24 23:25:26 +0300

    Removed unnecessary Fragment components

--- source/components/content/work-cover/types/social-media/SocialMedia.js
+++ source/components/content/work-cover/types/social-media/SocialMedia.js
@@ -14,29 +14,27 @@ import './SocialMedia.less'
 
 // Component: Content > Work Cover > Social Media (Type)
 const SocialMedia = props => (
-  <>
-    <div className="post">
-      <div className="information">
-        {props.data.platform &&
-          <Icon
-            name={props.data.platform.toLowerCase()}
-            color={rgbColor(props.data.colors.primary)} />
-        }
-        <div className="text">
-          <div className="publisher">{props.data.brand}</div>
-          <div className="time">2 hours ago</div>
-        </div>
-      </div>
-      <div className="content">
-        <Media
-          willCrop
-          type="image"
-          width={Math.ceil(45.5 * props.unitSize)}
-          height={Math.ceil(25 * props.unitSize)}
-          source={`/${props.data.slug}/cover/social-media.png`} />
+  <div className="post">
+    <div className="information">
+      {props.data.platform &&
+        <Icon
+          name={props.data.platform.toLowerCase()}
+          color={rgbColor(props.data.colors.primary)} />
+      }
+      <div className="text">
+        <div className="publisher">{props.data.brand}</div>
+        <div className="time">2 hours ago</div>
       </div>
     </div>
-  </>
+    <div className="content">
+      <Media
+        willCrop
+        type="image"
+        width={Math.ceil(45.5 * props.unitSize)}
+        height={Math.ceil(25 * props.unitSize)}
+        source={`/${props.data.slug}/cover/social-media.png`} />
+    </div>
+  </div>
 )
 
 // Properties
--- source/components/content/work-cover/types/website/Website.js
+++ source/components/content/work-cover/types/website/Website.js
@@ -10,27 +10,25 @@ import './Website.less'
 
 // Component: Content > Work Cover > Website (Type)
 const Website = props => (
-  <>
-    <div className="browser">
-      <div className="bar">
-        {['pink', 'yellow', 'green'].map(color =>
-          <div
-            key={color}
-            className="button"
-            style={{ backgroundColor: `var(--${color})` }}>
-          </div>
-        )}
-      </div>
-      <div className="site">
-        <Media
-          willCrop
-          type="image"
-          width={Math.ceil(47.75 * props.unitSize)}
-          height={Math.ceil(28 * props.unitSize)}
-          source={`/${props.data.slug}/cover/website.png`} />
-      </div>
+  <div className="browser">
+    <div className="bar">
+      {['pink', 'yellow', 'green'].map(color =>
+        <div
+          key={color}
+          className="button"
+          style={{ backgroundColor: `var(--${color})` }}>
+        </div>
+      )}
     </div>
-  </>
+    <div className="site">
+      <Media
+        willCrop
+        type="image"
+        width={Math.ceil(47.75 * props.unitSize)}
+        height={Math.ceil(28 * props.unitSize)}
+        source={`/${props.data.slug}/cover/website.png`} />
+    </div>
+  </div>
 )
 
 // Properties
