b1249221004dd1b15d2d3e509613272d1f739b1a
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2022-01-20 16:49:41 +0300

    Added Black Kite employment data and experience summary content

--- source/content/experience-summary/ExperienceSummary.js
+++ source/content/experience-summary/ExperienceSummary.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types'
 import AnadoluUniversity from './items/education/AnadoluUniversity'
 import MiddleEastTechnicalUniversity from './items/education/MiddleEastTechnicalUniversity'
 import Base64AI from './items/employment/Base64AI'
+import BlackKite from './items/employment/BlackKite'
 import DigitoyGames from './items/employment/DigitoyGames'
 import DocuAI from './items/employment/DocuAI'
 import FraktalProje from './items/employment/FraktalProje'
@@ -49,6 +50,8 @@ function ExperienceSummary (props) {
     switch (props.identifier) {
       case 'base64-ai':
         return <Base64AI />
+      case 'black-kite':
+        return <BlackKite />
       case 'digitoy-games':
         return <DigitoyGames />
       case 'docu-ai':
